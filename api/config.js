8243f5eb441536981556899dbc8440523dab5ec9
Yiotis Kaltsikis <giotisgr@gmail.com>
2020-04-27 17:12:53 +0200

    Add component level routing

--- api/config.js
+++ api/config.js
@@ -1,3 +1,7 @@
 export const SPACE_ID = "rjvs8luxqaq7";
 export const CDN_TOKEN = "5sXV5PsN1KplobLQKu-SsgxcMbZ0qAyWboZS-Uezyk4";
 export const PREVIEW_TOKEN = "R87VAzlgU6c9299zXdH06gbtooQxQnZwqeP98p21MEk";
+
+export const CONTENT_TYPE_IDS = {
+  BLOG_POST_TEASER: "blogPostTeaser",
+};
---
+++ components/ContentType.js
@@ -0,0 +1,28 @@
+/**
+ * This component will render the appropriate React component
+ * for any content type based on the content type name in the sys prop
+ */
+
+import React from "react";
+import get from "lodash/get";
+import { CONTENT_TYPE_IDS } from "../api/config";
+import { BlogPostTeaser } from "./BlogPostTeaser";
+
+function renderAppropriateComponent(entry) {
+  const contentTypeId = get(entry, "sys.contentType.sys.id");
+
+  switch (contentTypeId) {
+    case CONTENT_TYPE_IDS.BLOG_POST_TEASER:
+      return <BlogPostTeaser entry={entry} />;
+    default:
+      return null;
+  }
+}
+
+export class ContentType extends React.Component {
+  render() {
+    return renderAppropriateComponent(this.props.entry);
+  }
+}
+
+export default ContentType;
